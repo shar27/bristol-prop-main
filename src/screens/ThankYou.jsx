181d567956c71549bcfdba8d1552d38aa015cdf3
Shar <81372629+shar27@users.noreply.github.com>
2025-01-27 12:40:23 +0100

    form

---
+++ src/screens/ThankYou.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ThankYouHeader from '../components/Sections/ThankyouPage/ThankYouHeader'
+import TopNavbar from '../components/Nav/TopNavbar'
+
+function ThankYou() {
+  return (
+    <div>
+        <TopNavbar/>
+        <ThankYouHeader/>
+       
+    </div>
+  )
+}
+
+export default ThankYou
